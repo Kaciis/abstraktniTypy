6962abbc3df7834fffd31309f32885a56f0a1075
Jakub Kacálek <kacalek.kuba@post.cz>
2022-02-14 09:36:13 +0100

    edit

--- src/search/Program.cs
+++ src/search/Program.cs
@@ -1,13 +1,41 @@
 ﻿global using System;
 using System.Diagnostics;
 
-Stopwatch stopwatch = new Stopwatch();
 
-int[] pole = {1,2,3,4,5,6,7,8,9};
+int[] pole = new int[1000000];
 
-stopwatch.Start();
 
-Search.binarySearch(pole, 0, pole.Length-1, 7);
+Random rnd = new Random();
 
-stopwatch.Stop();
-Console.WriteLine(stopwatch.Elapsed);
+
+for(int i = 0 ;i < 1000000; i++){
+    pole[i] = rnd.Next(0, 100000);
+}
+
+
+
+Stopwatch stopwatchLinear = new Stopwatch();
+Stopwatch stopwatchBinary = new Stopwatch();
+
+#region LinearSearch
+stopwatchLinear.Start();
+
+Search.Linear(pole, 10000);
+
+stopwatchLinear.Stop();
+
+Console.WriteLine("Linear search : {0}", stopwatchLinear.Elapsed);
+
+#endregion
+
+Array.Sort(pole);
+
+#region BinarySearch
+stopwatchBinary.Start();
+
+
+Search.binarySearch(pole, 0, pole.Length-1, 0);
+
+stopwatchBinary.Stop();
+Console.WriteLine("binary search : {0}", stopwatchBinary.Elapsed);
+#endregion
